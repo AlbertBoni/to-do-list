b2dcf42a1170dbc5b50b3137f59845695ec1a50e
Albert Franklin Boni <albertfranklinboni@gmail.com>
2023-09-05 16:35:29 -0300

    alterando link deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/to-do-list"
+  base: "/to_do_list_react_vite"
 })
